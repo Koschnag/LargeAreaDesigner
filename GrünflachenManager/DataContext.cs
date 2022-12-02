daa2ab41438265a79ea999e899a669b9c77d9b0a
Andreas SOLSBACHER <andreas.solsbacher@soprasteria.com>
2022-12-02 12:31:49 +0100

    Projektdateien hinzufügen.

---
+++ GrünflachenManager/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using GrünflachenManager.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrünflachenManager;
+
+public class DataContext : DbContext
+{
+    public DbSet<Kunde> Kunden { get; set; }
+    public DbSet<Wunsch> Wünsche { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("\"Data Source=(localdb)\\\\MSSQLLocalDB;Initial Catalog=CarDB;Integrated Security=True;");
+        base.OnConfiguring(optionsBuilder);
+    }
+}
---
+++ GrünflachenManager/Models/Kunde.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrünflachenManager.Models;
+
+public class Kunde
+{
+    public string Vorname { get; set; } = "Neuer";
+    public string Name { get; set; } = "Kunde";
+    public string TelefonPrivat { get; set; } = string.Empty;
+    public string TelefonMobil { get; set; } = string.Empty;
+    public decimal Budget
+    {
+        get => Budget;
+        set
+        {
+            if (Budget == value)
+                return;
+
+            Budget = decimal.Round(value, 2, MidpointRounding.AwayFromZero);
+        }
+    }
+
+    public Wunsch? Wunsch { get; set; }
+
+    #region Addresse
+
+    public string Anschrift { get; set; } = string.Empty;
+    public string Ort { get; set; } = string.Empty;
+    public string Region { get; set; } = string.Empty;
+    public string PLZ { get; set; } = string.Empty;
+    public string Land { get; set; } = "Deutschland";
+
+    #endregion
+}
